400f71201039faa347d94342eef3b3d145fde21c
Simon Hatt <allestuetsmerweh@gmail.com>
2019-04-15 18:29:29 +0200

    Fix Testbench

--- build-docs.sh
+++ build-docs.sh
@@ -1,5 +1,4 @@
 npm run webpack-testbench
 mkdir -p docs/testbench
 cp testbench/build/testbench.min.js docs/testbench/
-cp testbench/index.html docs/testbench/
-cp testbench/styles.css docs/testbench/
+cp testbench/build/index.html docs/testbench/
