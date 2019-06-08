4c957c6e45a7d9d118133794371786f5f4d37422
Simon Hatt <allestuetsmerweh@gmail.com>
2019-06-08 23:05:06 +0200

    Introduce dependency cruiser
    close #6

--- build-docs.sh
+++ build-docs.sh
@@ -1,4 +1,7 @@
 npm run webpack-testbench
+mkdir -p docs/.circleci
+cp .circleci/config.yml docs/.circleci/
 mkdir -p docs/testbench
 cp testbench/build/testbench.min.js docs/testbench/
 cp testbench/build/index.html docs/testbench/
+./node_modules/dependency-cruiser/bin/dependency-cruise --config ./.dependency-cruiser.json --output-type dot src testbench | dot -T svg > docs/dependencygraph.svg
