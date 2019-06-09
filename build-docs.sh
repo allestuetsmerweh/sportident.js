6d06c607d7746b5161738009883bf9038f818fc4
Simon Hatt <allestuetsmerweh@gmail.com>
2019-06-09 08:35:00 +0200

    Fix docs

--- build-docs.sh
+++ build-docs.sh
@@ -1,7 +1,11 @@
-npm run webpack-testbench
 mkdir -p docs/.circleci
 cp .circleci/config.yml docs/.circleci/
+
 mkdir -p docs/testbench
+npm run webpack-testbench
 cp testbench/build/testbench.min.js docs/testbench/
 cp testbench/build/index.html docs/testbench/
-./node_modules/dependency-cruiser/bin/dependency-cruise --config ./.dependency-cruiser.json --output-type dot src testbench | dot -T svg > docs/dependencygraph.svg
+
+mkdir -p docs/generated
+./node_modules/dependency-cruiser/bin/dependency-cruise --config=./.dependency-cruiser.js --output-type=dot src | dot -T svg > docs/generated/dependencygraph-src.svg
+./node_modules/dependency-cruiser/bin/dependency-cruise --config=./.dependency-cruiser.js --output-type=dot testbench | dot -T svg > docs/generated/dependencygraph-testbench.svg
