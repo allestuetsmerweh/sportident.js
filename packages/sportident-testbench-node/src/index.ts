96fa81020a3580463b454025e7c213b465f1968d
Simon Hatt <hatt@google.com>
2019-11-25 20:19:14 +0100

    Enable backspace for testbench-node

--- packages/sportident-testbench-node/src/index.ts
+++ packages/sportident-testbench-node/src/index.ts
@@ -8,17 +8,27 @@ nodeUsbDriver.detect()
     .then((device: any) => {
         console.log('We have a device:', device);
 
+        const getCharBuffer: number[] = [];
         process.stdin.setRawMode(true);
         process.stdin.setEncoding('utf-8');
+        process.stdin.on('data', (char: string) => {
+            const charCode = char.charCodeAt(0);
+            if (charCode ===  127) {
+                getCharBuffer.push(8);
+                getCharBuffer.push(32);
+                getCharBuffer.push(8);
+                return;
+            }
+            getCharBuffer.push(charCode);
+        });
 
         const siShell = new Shell(
             {
                 getChar: () => {
-                    const char = process.stdin.read(1);
-                    if (!char) {
+                    if (getCharBuffer.length === 0) {
                         return undefined;
                     }
-                    return char.charCodeAt(0);
+                    return getCharBuffer.shift();
                 },
                 putChar: (char: number) => process.stdout.write(String.fromCharCode(char))
             },
@@ -29,6 +39,6 @@ nodeUsbDriver.detect()
         );
 
         siShell.run().then(() => {
-            device.close();
+            process.exit();
         });
     });
